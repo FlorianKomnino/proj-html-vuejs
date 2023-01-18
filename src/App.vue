960e17446f67a04e410f2d2fe6321f78c59ac3a7
FlorianKomnino <florian.komnino@outlook.it>
2023-01-18 15:46:01 +0100

    repo setup. vue + vite + bootstrap + sass. created store.js general.scss and _variables.scss

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import ProjHeader from './components/ProjHeader.vue';
+
+export default {
+
+  components: {
+    ProjHeader,
+  }
+}
+</script>
+
+<template>
+  <ProjHeader />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ProjHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'Header',
+
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
